862245266470fdbbb34b359783ce30c19cae95a8
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-03-23 15:51:10 +0530

    ugh

--- socket_rev/basic/client.c
+++ socket_rev/basic/client.c
@@ -3,12 +3,15 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <fcntl.h>
 
 int main(int args, char* argv[])
 {
     int sfd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server;
+    bzero(&server, sizeof(server));
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = htonl("127.0.0.1");
     server.sin_port = htons(8080);
--- socket_rev/basic/server.c
+++ socket_rev/basic/server.c
@@ -3,6 +3,8 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <math.h>
 #include <string.h>
 
